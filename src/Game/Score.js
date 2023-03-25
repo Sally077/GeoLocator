02e08dadb7d9b83bf5d0edc578d60085f8e334ec
Sally077 <sallyhedges77@gmail.com>
2023-03-25 11:05:16 +0000

    added files

---
+++ src/Game/Score.js
@@ -0,0 +1,9 @@
+import score from score.js;
+
+<ButtonGroup variant="contained" aria-label="outlined primary button group">
+  <Button>Home</Button>
+  <Button>Game</Button>
+  <Button>Score</Button>
+</ButtonGroup>
+
+
---
+++ src/Home/index.js
@@ -0,0 +1,5 @@
+<ButtonGroup variant="contained" aria-label="outlined primary button group">
+  <Button>Home</Button>
+  <Button>Game</Button>
+  <Button>Score</Button>
+</ButtonGroup>
