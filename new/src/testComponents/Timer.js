7421dcc01c98a39041c8259fbad9e8265539f7f7
Sally077 <sallyhedges77@gmail.com>
2023-03-28 19:17:11 +0100

    stored

--- new/src/testComponents/Timer.js
+++ new/src/testComponents/Timer.js
@@ -1,52 +1,61 @@
-
 import { useState, useEffect } from "react";
-import { Grid, Paper,  Button } from "@mui/material";
+import { Grid, Card, Button } from "@mui/material";
 import { render } from "react-dom";
 
-console.log("Test")
-
-
-{/*const that returns countDownTimer*/}
-function Timer()  {
-    const btnstyle = {margin: "8px 0", color: "custom color"}
-    const  paperStyle = {padding : 20, height : '20vh', width: 120, margin: "left"}
-    const [counter, setCounter] = useState(120);
-    const [isActive, setIsActive] =  useState(false)
-   
-
-    
-    const startTimer = ()=>{
-        setIsActive(true)
-    }
-    useEffect(() => {
-        
-        if(counter > 0 && isActive){
-         setTimeout(() => setCounter(counter - 1),1000);  
-        }
-    });
-
-    return (
-        <>
-        
-            <Grid>
-            <Paper elevation={10} style={paperStyle}>
-                <Grid align="left">
-                <h1>Timer</h1>
-                <div className="Timer"></div>
-        <div>Countdown: {counter}</div>
-            <Button type="Start"  style={btnstyle} onClick={()=> setIsActive(true)}>Start</Button>
-            </Grid>
-            </Paper>
-            </Grid>
-
-            </>
-
-            
-    );
+{
+  /*const that returns countDownTimer*/
 }
+function Timer() {
+  const btnstyle = { margin: "10px 0", color: "custom color", align: "center" };
+  const cardStyle = { padding: 20, height: 250, width: 220,  };
+  const cardStyle2 = { padding: 20, height: 250, width: 220,  };
+  const [counter, setCounter] = useState(120);
+  const [isActive, setIsActive] = useState(false);
+  // const [score, setScore] = useState(0)
 
+  const startTimer = () => {
+    setIsActive(true);
+  };
+  useEffect(() => {
+    if (counter > 0 && isActive) {
+      setTimeout(() => setCounter(counter - 1), 1000);
+    }
+  });
 
-const rootElement = document.getElementById("root");
-render(<Timer />, rootElement);
+  return (
+    <>
+      {/* Timer Card */}
+      <Grid>
+        <Card elevation={10} style={cardStyle}>
+          <Grid align="right">
+            <h1>Timer</h1>
+            <div className="Timer"></div>
+            <div>Countdown: {counter}</div>
+            <Button
+              type="Start"
+              style={btnstyle}
+              variant="contained"
+              color="success"
+              onClick={() => setIsActive(true)}
+            >
+              Start
+            </Button>
+          </Grid>
+        </Card>
+      </Grid>
+
+      {/* Score Card */}
+      <Grid>
+        <Card elevation={10} style={cardStyle2}>
+          <Grid align="left">
+            <h1>Score</h1>
+            <div className="Score"></div>
+            {/* <div>Score: {counter}</div> */}
+          </Grid>
+        </Card>
+      </Grid>
+    </>
+  );
+}
 
 export default Timer;
